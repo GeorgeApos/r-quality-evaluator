ca6ae9b4099d7b04aba113607c2c16dc1dd52ce3
George David Apostolidis <geoapostolidis999@gmail.com>
2023-05-16 18:53:15 +0300

    Added exec commands, needs improvement

--- backend/src/main/java/gr/uom/rqualityevaluator/service/FileService.java
+++ backend/src/main/java/gr/uom/rqualityevaluator/service/FileService.java
@@ -1,18 +1,22 @@
 package gr.uom.rqualityevaluator.service;
 
 import gr.uom.rqualityevaluator.models.FileAnalysis;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -30,10 +34,65 @@ public class FileService {
         }
         saveFileToDirectory(file, path);
 
+        setupREnvironment(fileAnalysis, path);
+
         executeAnalysis(fileAnalysis, path);
 
         deleteFileFromDirectory(file, path);
-        return new FileAnalysis();
+        return fileAnalysis;
+    }
+
+    private static void setupREnvironment(FileAnalysis file, Path path) {
+        executeCommand(file, "R", path);
+        executeCommand(file, "install.packages(\"styler\")", path);
+        executeCommand(file, "install.packages(\"lintr\")", path);
+        executeCommand(file, "install.packages(\"goodpractice\")", path);
+        executeCommand(file, "install.packages(\"cyclocomp\")", path);
+        executeCommand(file, "library(styler)", path);
+        executeCommand(file, "library(lintr)", path);
+        executeCommand(file, "library(goodpractice)", path);
+        executeCommand(file, "library(cyclocomp)", path);
+        executeCommand(file, "setwd(\"" + path + "\")", path);
+    }
+
+    private static void executeCommand(FileAnalysis file, String command, Path path) {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.directory(path.toFile());
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //edw bgainoun ta results
+                System.out.println(line);
+                processResults(file, line, command);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("Command executed successfully.");
+            } else {
+                System.out.println("Command failed with exit code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processResults(FileAnalysis file, String line, String command) {
+        List lintList = new ArrayList<>();
+        if(command.startsWith("lint(") && line.startsWith(System.getProperty("user.dir"))) {
+            lintList.add(line);
+        } else if(command.startsWith("cyclocomp(")) {
+            file.setCycloComplexity(Integer.parseInt(line));
+        } else if(command.startsWith("styler::style_file(")) {
+            file.setStylerResult(line);
+        } else if(command.startsWith("goodpractice::gp(") && line.startsWith(System.getProperty("user.dir"))) {
+
+        }
+        file.setLinterComments(lintList);
     }
 
     private static void executeAnalysis(FileAnalysis fileAnalysis, Path path) {
@@ -44,16 +103,19 @@ public class FileService {
     }
 
     private static void executeGoodPractises(FileAnalysis fileAnalysis, Path path) {
+        executeCommand(fileAnalysis,"goodpractice::gp(" + fileAnalysis.getName() + ")", path);
     }
 
     private static void executeStyler(FileAnalysis fileAnalysis, Path path) {
+        executeCommand(fileAnalysis,"styler::style_file(" + fileAnalysis.getName() + ")", path);
     }
 
     private static void executeCycloComplexity(FileAnalysis fileAnalysis, Path path) {
+        executeCommand(fileAnalysis,"cyclocomp(" + fileAnalysis.getName() + ")", path);
     }
 
     private static void executeLinter(FileAnalysis fileAnalysis, Path path) {
-        
+        executeCommand(fileAnalysis, "lint(" + fileAnalysis.getName() + ")", path);
     }
 
     private static void deleteFileFromDirectory(MultipartFile file, Path path) {
